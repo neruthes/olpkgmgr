8b47ca3ee7051809273c5dc28e26e50bed05265b
Neruthes <i@neruthes.xyz>
2023-06-29 13:14:37 +0000

    Regular update '2023-06-29T13:14:37+00:00'

--- make.sh
+++ make.sh
@@ -8,14 +8,6 @@ case $1 in
     build)
         ### Make directories
         mkdir -p build/bin dist
-        ### Make portable shared library
-        find lib -type f | sort | while read -r fn; do
-            cat "$fn"
-        done > build/spm-bash.lib.sh
-        ### Sync shared library
-        for binname in $BIN_TARGETS; do
-            rsync -av --delete "lib/" "src/$binname/001-lib/"
-        done
         ### Combining files
         for binname in $BIN_TARGETS; do
             echo "[INFO] Building target '$binname'"
@@ -29,7 +21,6 @@ case $1 in
         find build/bin -type f | while read -r binfn; do
             sudo install --verbose -m755 "$binfn" "/usr/local/bin/$(basename "$binfn")"
         done
-        sudo install --verbose build/spm-bash.lib.sh /usr/local/bin/spm-bash.lib.sh
         ;;
     easy)
         bash "$0" build
